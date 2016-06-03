f733f8decbf8767e0a74c9595a4f266050c5d737
SKS <821580550@qq.com>
2016-06-03 17:17:48 +0800

    //ceshi

---
+++ app/src/main/java/com/tangenhu/www/day30/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tangenhu.www.day30;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //测试git
+        int a = 101;
+
+    }
+}
