f4c6974bf705beff519a52a13a1a8eec8ba637a2
SKS <821580550@qq.com>
2016-06-03 17:33:35 +0800

    添加了一行新代码

--- app/src/main/java/com/tangenhu/www/day30/MainActivity.java
+++ app/src/main/java/com/tangenhu/www/day30/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tangenhu.www.day30;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
+
+    private TextView viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         //测试git
         int a = 101;
+        //第二次测试
+        initView();
+    }
 
+    private void initView(){
+        viewById = (TextView) findViewById(R.id.textView);
     }
 }
